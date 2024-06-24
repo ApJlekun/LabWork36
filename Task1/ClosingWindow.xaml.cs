c147075e67ffff6825711205a52ffbd3c604d609
КленинГолышев <KLENIN2000IGOR@GMAIL.COM>
2024-06-25 00:23:14 +0300

    master

---
+++ Task1/ClosingWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace Task1
+{
+    /// <summary>
+    /// Логика взаимодействия для ClosingWindow.xaml
+    /// </summary>
+    public partial class ClosingWindow : Window
+    {
+        public ClosingWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void YesButton_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+        }
+    }
+}
---
+++ Task1/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+
+namespace Task1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void NewWindowMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow window = new();
+            window.Show();
+        }
+
+        private void MainWindows_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            ClosingWindow closingWidow = new();
+            closingWidow.ShowDialog();
+            if (closingWidow.DialogResult != true)
+                e.Cancel = true;
+        }
+    }
+}
